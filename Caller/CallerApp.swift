2d7e3c07f8d93a84936f11bd7efdbdc47c777d4e
eric.yoo <eric@jtwolab.com>
2023-02-01 16:43:43 +0900

    Initial Commit

---
+++ Caller/CallerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CallerApp.swift
+//  Caller
+//
+//  Created by 유성웅 on 2023/02/01.
+//
+
+import SwiftUI
+
+@main
+struct CallerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
